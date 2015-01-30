4c5521a4eba12687878c619a89ad0b071b50917d
Eliza <eliza.danyi@gmail.com>
2015-01-29 23:29:05 -0500

    formatting test

--- Password Validation/Validate.java
+++ Password Validation/Validate.java
@@ -5,35 +5,34 @@ import java.util.Scanner;
 
 public class Validate {
 	public static void main(String[] args) {
-		
-		int attempts = 1;
-		while(attempts<4) {
-		Scanner input = new Scanner(System.in);
-	    
-		 System.out.print("Enter Password: ");
-	     String password = input.next();
-	       
-	        if (checkValid(password)) {
-	            System.out.println("Valid Password, Welcome");
-	        } 
-	        else {
-	            System.out.println("Invalid Password. " + "attempted " + attempts + "/3");
-	            attempts++;
-	        }
-	        
-	        if(attempts==4) {
-	        	System.out.println("Sorry, you have used all your password attempts.");
-	        }
-	    }
-	}
-	   
-	public static boolean checkValid(String password) {
-			if (password.equals("yes")) { 
-	            return true;
-	        } 
-	        else { 
-	             return false;
-	        }   
-    }
-}
 
+	int attempts = 1;
+	while(attempts<4) {
+	Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter Password: ");
+		String password = input.next();
+
+				if (checkValid(password)) {
+					System.out.println("Valid Password, Welcome");
+				}
+				else {
+					System.out.println("Invalid Password. " + "attempted " + attempts + "/3");
+							attempts++;
+				}
+
+				if(attempts==4) {
+						System.out.println("Sorry, you have used all your password attempts.");
+				}
+		}
+	}
+
+	public static boolean checkValid(String password) {
+				if (password.equals("yes")) {
+					return true;
+				}
+				else {
+					return false;
+				}
+	}
+}
