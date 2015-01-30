ef3a7471544937921720076436dbcb759c29b976
Eliza <eliza.danyi@gmail.com>
2015-01-29 23:41:59 -0500

    change requested by ser_pounce!

--- Password Validation/Validate.java
+++ Password Validation/Validate.java
@@ -2,37 +2,33 @@
 
 import java.util.Scanner;
 
-
 public class Validate {
+
   public static void main(String[] args) {
-
-  int attempts = 1;
-  while(attempts<4) {
-  Scanner input = new Scanner(System.in);
-
-    System.out.print("Enter Password: ");
-    String password = input.next();
-
-        if (checkValid(password)) {
-          System.out.println("Valid Password, Welcome");
-        }
-        else {
-          System.out.println("Invalid Password. " + "attempted " + attempts + "/3");
-              attempts++;
-        }
-
-        if(attempts==4) {
-            System.out.println("Sorry, you have used all your password attempts.");
-        }
-    }
+		Scanner input = new Scanner(System.in);
+  	int attempts = 1;
+  	while(attempts<4) {
+    	System.out.print("Enter Password: ");
+    	String password = input.next();
+    	if (checkValid(password)) {
+      	System.out.println("Valid Password, Welcome");
+    	}
+    	else {
+    		System.out.println("Invalid Password. " + "attempted " + attempts + "/3");
+      	attempts++;
+    	}
+    	if(attempts==4) {
+      	System.out.println("Sorry, you have used all your password attempts.");
+    	}
+  	}
   }
 
   public static boolean checkValid(String password) {
-        if (password.equals("yes")) {
-          return true;
-        }
-        else {
-          return false;
-        }
+    if (password.equals("yes")) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
