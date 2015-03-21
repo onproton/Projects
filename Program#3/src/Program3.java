b0d6b1bf47d3492bc2baaf2eb5a81ad053dd3b38
Eliza <emdanyi@gmail.com>
2015-03-20 23:01:14 -0400

    formatting
    reformatted some of the code

--- Program#3/src/Program3.java
+++ Program#3/src/Program3.java
@@ -3,7 +3,7 @@
 * Colorado State University – ITS-320 – Basic Programming 
 *
 * This program calculates the totals order retail amounts for a selection of 5 products and quantities entered
-* as puchased. 
+* as puchased. Practicing java while and for loops as well as switches in the Eclipse IDE.
 *
 * Programmed by: Eliza Danyi
 * 
@@ -14,11 +14,9 @@
 import java.util.Scanner; //scanner class import
 import java.text.NumberFormat;
 
-public class Program3 
-{
+public class Program3 {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		
 		int productNo; //stores the product number entered
 		int unitsSold; //stores the number of units sold entered
@@ -29,16 +27,14 @@ public class Program3
 		Scanner keyboard = new Scanner(System.in);
 
 		//Creates a for loop that repeats a known number of times (5) by incrementing the loop control variable "counter".
-		for(int counter = 0; counter < 5; ++counter) 
-		{
+		for(int counter = 0; counter < 5; ++counter) {
 		
 		// Get the product number.
 		System.out.print("Please enter the product number >> ");
 		productNo = keyboard.nextInt();
 
 		//Validate the productNumber entry and prompt for new value if invalid (< 0 or >5).
-		while(productNo <=0 || productNo > 5) 
-		{
+		while(productNo <=0 || productNo > 5) {
 			System.out.println(productNo + " is not a valid product number. Please enter an integer between 1 and 5 >> ");
             productNo = keyboard.nextInt();
 		}
@@ -48,15 +44,13 @@ public class Program3
 		unitsSold = keyboard.nextInt();
 		
 		//Validate the units sold entry and prompt for new value if invalid (<0 or >5).
-		while(unitsSold <=0 || unitsSold > 5) 
-		{
+		while(unitsSold <=0 || unitsSold > 5) {
 			System.out.println(unitsSold + " is not a valid number of units. Please enter an integer between 1 and 5 >> ");
 			unitsSold = keyboard.nextInt();
 		}
 		
 		//switch that determines the price per unit for the product number entered and multiplies it by the number of units sold and sets linePrice to that value.
-		switch(productNo) 
-		{
+		switch(productNo) {
 		case 1:
 			linePrice = unitsSold * 2.98;
 			break;
@@ -82,9 +76,6 @@ public class Program3
 		
 		//Prints running total.
 		System.out.println("The running total for this order is: $" + totalPrice);
-		
-		
-		
 		}
 		
 		//Prints the final total of the 5 lines.
