eb3e15574254e44dd7bfccd063c3d2445e9d274a
Eliza <emdanyi@gmail.com>
2015-03-22 00:43:23 -0400

    spacing
    Xcode not registering spacing correctly

--- Program#2/src/BankAccount.java
+++ Program#2/src/BankAccount.java
@@ -12,44 +12,44 @@
  */
 
 public class BankAccount {
-	private double balance;    // Account balance
-	private double interestRate; // Interest rate
-	private double interest;   // Interest earned
-	/**
-	 * The constructor initializes the balance
-	 * and interestRate fields with the values
-	 * passed to startBalance and intRate. The
-	 * interest field is assigned to 0.0.
-	 */
-	public BankAccount(double startBalance, double intRate) {
-		balance = startBalance;
+    private double balance;    // Account balance
+    private double interestRate; // Interest rate
+    private double interest;   // Interest earned
+    /**
+     * The constructor initializes the balance
+     * and interestRate fields with the values
+     * passed to startBalance and intRate. The
+     * interest field is assigned to 0.0.
+     */
+    public BankAccount(double startBalance, double intRate) {
+        balance = startBalance;
         //to get monthlyRate, the annual rate input, intRate is divided
         //by 12 months and the result is then divided by 100 to return a %.
-		double monthlyRate = (intRate/12)/100;
+        double monthlyRate = (intRate/12)/100;
         
         //this sets interestRate to the calculated monthly rate above.
         interestRate = monthlyRate;
-		interest = 0.0;
-	}
-	// The deposit method adds the parameter amount to the balance field.
-	public void deposit(double amount) {
-		balance += amount;
-	}
-	// The withdraw method subtracts the parameter amount from the balance field.
-	public void withdraw(double amount) {
-		balance -= amount;
-	}
-	// The addInterest method adds the interest for the month to the balance field.
-	public void addInterest() {
-		interest = balance * interestRate;
-		balance += interest;
-	}
-	// The getBalance method returns the value in the balance field.
-	public double getBalance() {
-		return balance;
-	}
-	// The getInterest method returns the value in the interest field.
-	public double getInterest() {
-		return interest;
-	}
+        interest = 0.0;
+    }
+    // The deposit method adds the parameter amount to the balance field.
+    public void deposit(double amount) {
+        balance += amount;
+    }
+    // The withdraw method subtracts the parameter amount from the balance field.
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+    // The addInterest method adds the interest for the month to the balance field.
+    public void addInterest() {
+        interest = balance * interestRate;
+        balance += interest;
+    }
+    // The getBalance method returns the value in the balance field.
+    public double getBalance() {
+        return balance;
+    }
+    // The getInterest method returns the value in the interest field.
+    public double getInterest() {
+        return interest;
+    }
 }
