d7b62f85c9c080d75853ad8b7953238617496133
Eliza <emdanyi@gmail.com>
2015-03-22 00:41:06 -0400

    formatting
    spacing was off

--- Program#2/src/BankAccount.java
+++ Program#2/src/BankAccount.java
@@ -15,14 +15,12 @@ public class BankAccount {
 	private double balance;    // Account balance
 	private double interestRate; // Interest rate
 	private double interest;   // Interest earned
-
 	/**
 	 * The constructor initializes the balance
 	 * and interestRate fields with the values
 	 * passed to startBalance and intRate. The
 	 * interest field is assigned to 0.0.
 	 */
-
 	public BankAccount(double startBalance, double intRate) {
 		balance = startBalance;
         //to get monthlyRate, the annual rate input, intRate is divided
@@ -33,28 +31,23 @@ public class BankAccount {
         interestRate = monthlyRate;
 		interest = 0.0;
 	}
-    
 	// The deposit method adds the parameter amount to the balance field.
 	public void deposit(double amount) {
 		balance += amount;
 	}
-    
 	// The withdraw method subtracts the parameter amount from the balance field.
 	public void withdraw(double amount) {
 		balance -= amount;
 	}
-    
 	// The addInterest method adds the interest for the month to the balance field.
 	public void addInterest() {
 		interest = balance * interestRate;
 		balance += interest;
 	}
-    
 	// The getBalance method returns the value in the balance field.
 	public double getBalance() {
 		return balance;
 	}
-    
 	// The getInterest method returns the value in the interest field.
 	public double getInterest() {
 		return interest;
