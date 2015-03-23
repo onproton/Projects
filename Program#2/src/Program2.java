8c7d69c0d965ee1983762c4ba23c0460d22776e7
Eliza <emdanyi@gmail.com>
2015-03-22 21:13:32 -0400

    clarity & formatting
    added comments and formatted for clarity - added pay amount to the
    printout for using the BankAccount.deposit method

--- Program#2/src/Program2.java
+++ Program#2/src/Program2.java
@@ -1,68 +1,60 @@
 /**
 *
-* Colorado State University – ITS-320 – Basic Programming 
+* Colorado State University – ITS-320: Java Programming
 *
-* This program demonstrates using the BankAccount class.
+* This program demonstrates using the BankAccount class to simulate a bank
+* account and demonstrates how its methods can be used to manipulate the object.
 *
-* (Taken from "Starting Out with Java - Early Objects 
+* (Adapted by Eliza Danyi from "Starting Out with Java - Early Objects
 * (Third Edition) by Tony Gaddis, 2008 by Pearson Educ.)
-*
-* Programmed by: Eliza Danyi
-* 
-* Date: October, 18 2014
 *
 */
 
-import java.util.Scanner;    // Needed for the Scanner class
-import java.text.DecimalFormat; // Needed for 2 decimal place amounts
+import java.util.Scanner;
+import java.text.DecimalFormat; // decimal number formatting
 
-public class Program2
-{
-	public static void main(String[] args)
-	{
-		BankAccount account; // To reference a BankAccount object
-		double balance,    // The account's starting balance
-		interestRate, // The monthly interest rate
-		pay,      // The user's pay
-		cashNeeded;  // The amount of cash to withdraw
+public class Program2 {
+    public static void main(String[] args) {
+        BankAccount account;  // To reference a BankAccount object
+        double balance,       // The account's starting balance
+		interestRate,         // The monthly interest rate
+		pay,                  // The user's pay
+		cashNeeded;           // The amount of cash to withdraw
 
 		// Create a Scanner object for keyboard input.
 		Scanner keyboard = new Scanner(System.in);
-
-		// Create an object for dollars and cents
+		// Create an object for formatting dollars and cents
 		DecimalFormat formatter = new DecimalFormat ("#0.00");
 
-		// Get the starting balance.
+		// Prompt user for the starting balance and populate balance variable
 		System.out.print("What is your account's " + "starting balance? ");
 		balance = keyboard.nextDouble();
-
-		// Get the monthly interest rate.
+		// Prompt user for the monthly interest rate and populate interestRate variable
 		System.out.print("What is your annual interest rate? ");
 		interestRate = keyboard.nextDouble();
-
-		// Create a BankAccount object.
+		// Create a BankAccount object 'account' with the entered balance and interestRate
 		account = new BankAccount(balance, interestRate);
-
-		// Get the amount of pay for the month.
+		// Prompt for amount of pay for the month & populate 'pay' variable
 		System.out.print("How much were you paid this month? ");
 		pay = keyboard.nextDouble();
 
-		// Deposit the user's pay into the account.
-		System.out.println("We will deposit your pay " + "into your account.");
-		account.deposit(pay);
-		System.out.println("Your current balance is $" + formatter.format( account.getBalance()  ));
+		// Deposit the user's pay into the account using the deposit method of the bank acct class
+		System.out.println("We will deposit " + pay + " into your account.");
+        account.deposit(pay);
+        // prints the balance using the getBalance method and formats it using the DecimalFormat 'formatter'
+        System.out.println("Your current balance is $" + formatter.format( account.getBalance()  ));
 
-		// Withdraw some cash from the account.
+		// Prompts user for withdraw amount
 		System.out.print("How much would you like " + "to withdraw? ");
-		cashNeeded = keyboard.nextDouble();
-		account.withdraw(cashNeeded);
+		// Populates the cashNeeded var with entered amount
+        cashNeeded = keyboard.nextDouble();
+		// uses the withdraw method of the bank account class to withdraw amount
+        account.withdraw(cashNeeded);
 		
-		// Add the monthly interest to the account.
+		// Add the monthly interest to the account using the addInterest method
 		account.addInterest();
-		
-		// Display the interest earned and the balance.
-		System.out.println("This month you have earned $" + formatter.format( account.getInterest() ) +
-				" in interest.");
-		System.out.println("Now your balance is $" + formatter.format( account.getBalance() ) );
-	}
+		// Display the interest earned and the balance formatted in DecimalFormat
+        System.out.println("This month you have earned $" + formatter.format( account.getInterest() ) + " in interest.");
+        System.out.println("Now your balance is $" + formatter.format( account.getBalance() ) );
+    }
 }
