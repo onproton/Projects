d354e48d6a34ac250be86034a869d64123949eb3
Eliza <emdanyi@gmail.com>
2015-03-23 23:46:12 -0400

    clarity & formatting
    -

--- Program#4/src/MidtermExam.java
+++ Program#4/src/MidtermExam.java
@@ -12,30 +12,34 @@
 */
 
 
-public class MidtermExam 
-{
-
-	public static int[] numbers; //declares the numbers array.
+public class MidtermExam {
+    public static int[] numbers; //declares the numbers array.
 	
-	public static void main(String[] args) {
-
-		numbers = new int[25];   //allocate space for the numbers array with room for 25 int values
-		
-        for(int i = 0; i <25; i++)   //for loop to initialize values 1-25 into the numbers array,
-           numbers[i] = i + 1;    //the value will be one more than its index number in [], i increments up to 25 (but does not include 25).
+    public static void main(String[] args) {
+        
+        //allocate space for the numbers array with room for 25 int values
+        numbers = new int[25];
+        //for loop to initialize values 1-25 into the numbers array,
+        for(int i = 0; i <25; i++) {
+            numbers[i] = i + 1;    //i increments up to 25 (but does not include 25).
+        }
         
         printNumbers();   //calls the printNumbers method to print the array on 5 lines
-	}
+    }
 	
-	public static void printNumbers() {  //this is the method that defines how to print the array
-       
-    	   int i;   //declares local variable i
+    public static void printNumbers() {  //this is the method that defines how to print the array
+        int i;   //declares local variable i
     	   
-           for(i = 1; i <= 25; i++) {  //for loop incrementing i up to 25
-               if (i % 5 != 0)   //determines if i is divisible by 5
-                   System.out.print(numbers[i-1]+",");   //if i is not divisible by 5, print element with index i-1 (to account for the indexes starting at 0), followed by a comma but not a new line
-               else
-                   System.out.println(numbers[i-1]);   //if i is divisible by 5, print the element with index i-1, but without a comma and followed by a new line
-    	   }
+        for(i = 1; i <= 25; i++) {  //for loop incrementing i up to 25
+            if (i % 5 != 0) {  //determines if i is divisible by 5
+                //if i is not divisible by 5, print element with index i-1, followed by a comma
+                System.out.print(numbers[i-1]+",");
+            }
+            else {
+                //if i is divisible by 5, print the element with index i-1, followed by a new line
+                System.out.println(numbers[i-1]);
+            }
+        }
     }
 }
+
